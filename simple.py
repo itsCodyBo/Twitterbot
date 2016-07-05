45da3834919c18c81f55a261dcf7385656080f24
Cody Bordelon <codylbordelon@gmail.com>
2016-07-05 18:41:58 -0500

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,10 +1,10 @@
 from twython import Twython, TwythonError
 import time
 
-APP_KEY = 'kOWO9eym53xx4KqaPYwu2TFPP'
-APP_SECRET = 'odCoTzWoPUYpGhjZ1DuGjeXHKgZP5A3oIC5h52A7OnbiSMDRRv'
-OAUTH_TOKEN = '857527369-wwGb79so2nnjCrYaiSdBE4ZCt9lfFtljIMA1VOWJ'
-OAUTH_TOKEN_SECRET = 'UjyeKJyH9WZk5BWm1jRRCzFltaAqUowTtDcjb0R8owOCN'
+APP_KEY = '#'
+APP_SECRET = '#'
+OAUTH_TOKEN = '#'
+OAUTH_TOKEN_SECRET = '#'
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
